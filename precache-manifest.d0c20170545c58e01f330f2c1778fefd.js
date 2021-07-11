f4df1e3c37d78fc4dbc172fb8e7c54e3a5617fd0
hatimpatanwala <hatimjuzer2@gmail.com>
2021-07-11 15:40:55 +0530

    Updates

---
+++ precache-manifest.d0c20170545c58e01f330f2c1778fefd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1863e7711d9eb43c4e35f16d598167b7",
+    "url": "/signuplogin/index.html"
+  },
+  {
+    "revision": "a7bacadeaf250003ecce",
+    "url": "/signuplogin/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ff02665e09585cad7ba2",
+    "url": "/signuplogin/static/css/main.203e2753.chunk.css"
+  },
+  {
+    "revision": "a7bacadeaf250003ecce",
+    "url": "/signuplogin/static/js/2.1a30426a.chunk.js"
+  },
+  {
+    "revision": "ff02665e09585cad7ba2",
+    "url": "/signuplogin/static/js/main.f54cd4c5.chunk.js"
+  },
+  {
+    "revision": "44ea09b7eac62336024a",
+    "url": "/signuplogin/static/js/runtime-main.1041019a.js"
+  }
+]);
