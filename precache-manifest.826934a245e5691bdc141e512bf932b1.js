4e1bfe78335ec7fffef972e39a08a24ea6cd1da2
hatimpatanwala <hatimjuzer2@gmail.com>
2021-07-11 16:16:01 +0530

    Updates

--- precache-manifest.826934a245e5691bdc141e512bf932b1.js
+++ precache-manifest.826934a245e5691bdc141e512bf932b1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1863e7711d9eb43c4e35f16d598167b7",
+    "revision": "07e7d5bae05ea11745b2625a4ddd58ba",
     "url": "/signuplogin/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/signuplogin/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "ff02665e09585cad7ba2",
+    "revision": "e2f3a495a64090812b26",
     "url": "/signuplogin/static/css/main.203e2753.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/signuplogin/static/js/2.1a30426a.chunk.js"
   },
   {
-    "revision": "ff02665e09585cad7ba2",
-    "url": "/signuplogin/static/js/main.f54cd4c5.chunk.js"
+    "revision": "e2f3a495a64090812b26",
+    "url": "/signuplogin/static/js/main.6ab45d23.chunk.js"
   },
   {
     "revision": "44ea09b7eac62336024a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/signuplogin/precache-manifest.d0c20170545c58e01f330f2c1778fefd.js"
+  "/signuplogin/precache-manifest.826934a245e5691bdc141e512bf932b1.js"
 );
 
 self.addEventListener('message', (event) => {
