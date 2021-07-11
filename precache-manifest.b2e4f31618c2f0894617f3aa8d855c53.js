6ff647a9229a1b70231284a12df34bf9edfbf09c
hatimpatanwala <hatimjuzer2@gmail.com>
2021-07-11 16:03:52 +0530

    Updates

--- precache-manifest.b2e4f31618c2f0894617f3aa8d855c53.js
+++ precache-manifest.b2e4f31618c2f0894617f3aa8d855c53.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1863e7711d9eb43c4e35f16d598167b7",
+    "revision": "775682a1c83b5d2f2bcc5809fa69c9a4",
     "url": "/signuplogin/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/signuplogin/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "ff02665e09585cad7ba2",
+    "revision": "c22af62ce1b2468fd9d1",
     "url": "/signuplogin/static/css/main.203e2753.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/signuplogin/static/js/2.1a30426a.chunk.js"
   },
   {
-    "revision": "ff02665e09585cad7ba2",
-    "url": "/signuplogin/static/js/main.f54cd4c5.chunk.js"
+    "revision": "c22af62ce1b2468fd9d1",
+    "url": "/signuplogin/static/js/main.b860863e.chunk.js"
   },
   {
     "revision": "44ea09b7eac62336024a",
